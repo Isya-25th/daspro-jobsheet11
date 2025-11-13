28cb62378580e0fb1b7b0ff593b6a77bb4cc5209
Isya-25th <isyaputra.r25th@gmail.com>
2025-11-13 13:18:26 +0700

    Modifikasi 1

--- BioskopWithScanner13.java
+++ BioskopWithScanner13.java
@@ -3,25 +3,58 @@ import java.util.Scanner;
 public class BioskopWithScanner13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String [][] penonton = new String[4][2];
+        String[][] penonton = new String[4][2];
+
         while (true) {
-            System.out.print("Masukkan nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            int baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            int kolom = sc.nextInt();
+            System.out.println("\nMenu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu (1-3): ");
+            int pilihan = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1] [kolom-1] = nama;
+            switch (pilihan) {
+                case 1:
+                    while (true) {
+                        System.out.print("Masukkan nama: ");
+                        String nama = sc.nextLine();
+                        System.out.print("Masukkan baris (1-4): ");
+                        int baris = sc.nextInt();
+                        System.out.print("Masukkan kolom (1-2): ");
+                        int kolom = sc.nextInt();
+                        sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            String next = sc.nextLine();
+                        penonton[baris - 1][kolom - 1] = nama;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                        System.out.print("Input penonton lainnya? (y/n): ");
+                        String next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("\nDaftar Penonton:");
+                    for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            String namaPenonton = (penonton[i][j] != null) ? penonton[i][j] : "";
+                            System.out.print("Baris " + (i + 1) + " Kolom " + (j + 1) + ": " + namaPenonton + "\t");
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Terima kasih. Sampai jumpa!");
+                    sc.close();
+                    return;
+
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih 1-3.");
             }
         }
-        sc.close();
     }
 }
